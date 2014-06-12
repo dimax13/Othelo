9c11fc9c2b490f48f23618ab8b68edce269f675b
dimax13 <dai13.freeman@gmail.com>
2014-06-12 17:44:01 +0900

    ViewModelのプロパティを実装した

--- Othelo/Othelo/View/MainWindow.xaml.cs
+++ Othelo/Othelo/View/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Othelo
+namespace Othelo.View
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
---
+++ Othelo/Othelo/ViewModel/GameInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Othelo.Model;
+using Othelo.Common;
+
+namespace Othelo.ViewModel
+{
+    class GameInfo : ViewModelBase
+    {
+        #region Property
+        private Board _board;
+        public Board Board { get { return _board; } set { _board = value; RaisePropertyChanged("Board"); } }
+
+        private int _blackScore = 2;
+        public int BlacScore
+        {
+            get { return _blackScore; }
+            set { _blackScore = value; RaisePropertyChanged("BlacScore"); }
+        }
+
+        private int _whiteScore = 2;
+        public int WhiteScore
+        {
+            get { return _whiteScore; }
+            set { _whiteScore = value; RaisePropertyChanged("WhiteScore"); }
+        }
+
+        #endregion
+    }
+}
